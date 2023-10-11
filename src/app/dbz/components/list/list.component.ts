d09540d083b35914b808cd3095d660a9114b7c54
Jonathan Valderrabano <jvalderrabano@xcaret.com>
2023-10-11 13:52:58 -0500

    Trabajando y aprendiendo con formularios

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
 
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+
+  onDeleteCharacter(index: number): void {
+    //TODO:
+    this.onDelete.emit(index);
+  }
 }
